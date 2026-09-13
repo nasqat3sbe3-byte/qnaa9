40b8b00618d0642474d1c223d72f2cf2228e3d40
nasqat3sbe3-byte <nas.qat3sbe3@gmail.com>
2026-09-13 16:02:34 +0300

    Add database package module

---
+++ app/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from .config import settings
+
+kwargs = {"check_same_thread": False} if settings.database_url.startswith("sqlite") else {}
+engine = create_engine(settings.database_url, pool_pre_ping=True, connect_args=kwargs)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+def init_db():
+    from . import models  # noqa: F401
+    Base.metadata.create_all(engine)
